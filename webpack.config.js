622d36cee4ba870108c5ca800d6220a73560a2a1
OveR1st <315overdose@gmail.com>
2020-12-29 00:07:45 +0200

    Logic Env Production and Development

--- webpack.config.js
+++ webpack.config.js
@@ -1,67 +1,98 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-module.exports = {
-  mode: "development",
 
-  module: {
-    rules: [
-      // Loading Babel
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader' // когда 1 лоадер можно без [] use
-      },
-      // Loading images
-      {
-        test: /\.(png|jpg|gif|ico)$/,
-        use: [
-          { 
-            loader: 'file-loader',
-            options: {
-              outputPath: 'images',
-              name: '[name]-[sha1:hash:7].[ext]'
-            }
-          }
-        ]
-      },
-      // Loading fonts
-      {
-        test: /\.(ttf|otf|eot|woff|woff2)$/,
-        use: [
-          { 
-            loader: 'file-loader',
-            options: {
-              outputPath: 'fonts',
-              name: '[name].[ext]'
-            }
-          }
-        ]
-      },
-      // Loading CSS
-      {
-        test: /\.(css)$/,
-        use: [ MiniCssExtractPlugin.loader, 'css-loader' ] // без вложеных {} с loader key 
-      },
-       // Loading SASS/SCSS
-      {
-        test: /\.(s[ca]ss)$/,
-        use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ]// идет к обработка и передает выше по цепи
-      }
+
+module.exports = (env = {}) => {
+ 
+
+  const isProd = env.production === true;
+  const isDev = env.NODE_ENV === 'local';
+
+  const getStyleLoaders = () => {
+    return [
+      isProd ? MiniCssExtractPlugin.loader : 'style-loader',
+      'css-loader'
     ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title : 'Hello World',
-      buildTime: new Date().toString(),
-      template: 'public/index.html'
-    }),
-    new MiniCssExtractPlugin({
-      filename: 'main-[hash:8].css' // хеш - измение при каждой сборке 
-    })
-  ],
+  }
+
+  const getPlugins = () => {
+    const plugins = [
+      new HtmlWebpackPlugin({
+        title : 'Hello World',
+        buildTime: new Date().toString(),
+        template: 'public/index.html'
+      })
+    ];
+
+    if (isProd) {
+      plugins.push(new MiniCssExtractPlugin({
+        filename: 'main-[hash:8].css' // хеш - измение при каждой сборке 
+      })
+      )
+    };
+
+    return plugins; 
+  };
+
+  return {
+    mode: isProd ? 'production' : isDev && 'local',
+
+    output: {
+      filename: isProd ? 'main-[hash:8].js' : undefined
+    },
+
+    module: {
+      rules: [
+        // Loading Babel
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader' // когда 1 лоадер можно без [] use
+        },
+        // Loading images
+        {
+          test: /\.(png|jpg|gif|ico)$/,
+          use: [
+            { 
+              loader: 'file-loader',
+              options: {
+                outputPath: 'images',
+                name: '[name]-[sha1:hash:7].[ext]'
+              }
+            }
+          ]
+        },
+        // Loading fonts
+        {
+          test: /\.(ttf|otf|eot|woff|woff2)$/,
+          use: [
+            { 
+              loader: 'file-loader',
+              options: {
+                outputPath: 'fonts',
+                name: '[name].[ext]'
+              }
+            }
+          ]
+        },
+        // Loading CSS
+        {
+          test: /\.(css)$/,
+          use: getStyleLoaders() // без вложеных {} с loader key 
+        },
+          // Loading SASS/SCSS
+        {
+          test: /\.(s[ca]ss)$/,
+          use: [ ...getStyleLoaders(), 'sass-loader' ]// идет к обработка и передает выше по цепи
+        }
+      ]
+    },
   
-  devServer: {
-    open: true
+    plugins: getPlugins(),
+
+    devServer: {
+      open: true // открытие браузера после старта сервера
+    }
   }
 }
 
