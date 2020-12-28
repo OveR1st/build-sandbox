aea89e2c35ddc47f8601c1e999a7df9208591056
OveR1st <315overdose@gmail.com>
2020-12-28 14:22:44 +0200

    New loader

--- webpack.config.js
+++ webpack.config.js
@@ -3,14 +3,28 @@ module.exports = {
 
   module: {
     rules: [
+      // Loading images
       {
-        test: /\.png$/,
+        test: /\.(png|jpg|gif|ico)$/,
         use: [
           { 
             loader: 'file-loader',
             options: {
               outputPath: 'images',
               name: '[name]-[sha1:hash:7].[ext]'
+            }
+          }
+        ]
+      },
+      {
+      // Loading fonts
+        test: /\.(ttf|otf|eot|woff|woff2)$/,
+        use: [
+          { 
+            loader: 'file-loader',
+            options: {
+              outputPath: 'fonts',
+              name: '[name].[ext]'
             }
           }
         ]
