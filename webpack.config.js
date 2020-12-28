2b9fbab15af44a733765501c6c2733647c1a72e8
OveR1st <315overdose@gmail.com>
2020-12-28 21:34:15 +0200

    Plugin - работают со всем проектом в отличии от Loader'ов

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: "development",
 
   module: {
     rules: [
-
+      // Loading Babel
       {
         test: /\.js$/,
         exclude: /node_modules/,
@@ -22,8 +23,8 @@ module.exports = {
           }
         ]
       },
-      {
       // Loading fonts
+      {
         test: /\.(ttf|otf|eot|woff|woff2)$/,
         use: [
           { 
@@ -35,16 +36,21 @@ module.exports = {
           }
         ]
       },
+      // Loading CSS
       {
-        // Loading CSS
         test: /\.(css)$/,
         use: [ 'style-loader', 'css-loader' ] // без вложеных {} с loader key 
       },
+       // Loading SASS/SCSS
       {
-        // Loading SASS/SCSS
         test: /\.(s[ca]ss)$/,
         use: [ 'style-loader', 'css-loader', 'sass-loader' ]// идет к обработка и передает выше по цепи
       }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'public/index.html'
+    })
+  ]
 }
