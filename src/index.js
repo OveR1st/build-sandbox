58ac1c588f8502e7f184ed935700021543f6686b
OveR1st <315overdose@gmail.com>
2020-12-28 14:35:32 +0200

    Babel loader in webpack config

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
-import Log from './log.js';
-import Calc from './calc.js';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import img from './react.png';
+const App = () => <h1>Hello World!</h1>
 
-const calc = new Calc();
-const log = new Log();
-
-log.log(calc.add(1,2,3));
-
-const el = document.createElement('img');
-el.src = img;
-document.body.appendChild(el);
+ReactDOM.render(<App />,
+  document.getElementById('root'));
